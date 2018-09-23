67a9b78950bc83f9418fcfd1b9cfb1d79cec7435
Cole Agard <ctagard19@gmail.com>
2018-09-23 12:42:35 -0700

    Newest update, provided new normal curve function to streamline code.

--- Distance-function-MASTER.py
+++ Distance-function-MASTER.py
@@ -1,3 +1,19 @@
 #Cole Agard
 #Icepick Analytics MASTER 
 #9/23/2018
+from numpy import *
+from sympy import *
+from scipy import *
+from Normal_functionfinder import normal
+x, y, z = symbols("x y z") #Define the symbols that we will be using
+
+functionone = input("What is the first function? Please use 'y' when typing symbolic equation ") #f(x), user defined
+print("f(x) =  " + functionone + " ")
+functiontwo = input("What is the second function? Please use 'y' when typing symbolic equation ")#g(x), user defined
+
+print("g(x) = " + functiontwo + "")
+function1real = sympify(functionone)
+
+function2real = sympify(functiontwo)
+
+print(normal(function1real))
---
+++ Normal_functionfinder.py
@@ -0,0 +1,21 @@
+#Cole Agard
+#function that determines ALL equations normal to f(x), represented by function1
+#To change number of equations, change the top bound of the range in line 17
+from sympy import *
+from numpy import *
+import numpy as np
+from scipy import *
+from sympy import diff
+
+x, y, z = symbols("x y z")
+init_printing(use_unicode=True)
+
+
+def normal(function1):
+    a = (-1/diff(function1, y)) * x + (function1 + y/diff(function1, y))
+    results = []
+    for i in range(1, 101):
+        j = a.subs(y, i)
+        results.append(a.subs(y, i))
+    return results;
+
