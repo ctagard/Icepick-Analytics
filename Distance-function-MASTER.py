ebd930e6419a247c68e394e97badff2597fee411
Cole Agard <ctagard19@gmail.com>
2018-09-24 06:13:02 -0700

    New updates

--- Distance-function-MASTER.py
+++ Distance-function-MASTER.py
@@ -5,15 +5,25 @@ from numpy import *
 from sympy import *
 from scipy import *
 from Normal_functionfinder import normal
+from Definitionsfunctions import functionone,functiontwo
+from Rootfinder import root_finder
+from PointX2solver import solver
+from finalDistance import final_distance
 x, y, z = symbols("x y z") #Define the symbols that we will be using
 
-functionone = input("What is the first function? Please use 'y' when typing symbolic equation ") #f(x), user defined
-print("f(x) =  " + functionone + " ")
-functiontwo = input("What is the second function? Please use 'y' when typing symbolic equation ")#g(x), user defined
+a = str(functionone())
+b = str(functiontwo())
+function1real = sympify(a)
+function2real = sympify(b)
+def funct1(y):
+    return sympify(a)
+def funct2(y):
+    return sympify(b)
 
-print("g(x) = " + functiontwo + "")
-function1real = sympify(functionone)
+allnormallines = normal(function1real)
+print(allnormallines)
+solutionsofroots = root_finder(allnormallines, function2real)
+x2 = solver(solutionsofroots)
+finaldistance = final_distance(x2,funct1(y),funct2(y))
+print(finaldistance)
 
-function2real = sympify(functiontwo)
-
-print(normal(function1real))
